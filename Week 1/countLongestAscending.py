bf74a4e59ceb99ee18e30a0637094d190a7e77fa
hyerynn0521 <52299631+hyerynn0521@users.noreply.github.com>
2021-06-10 22:39:48 -0700

    Create countLongestAscending.py

---
+++ Week 1/countLongestAscending.py
@@ -0,0 +1,18 @@
+#
+# Complete the 'countLongestAscending' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY number_array as parameter.
+#
+def countLongestAscending(number_array):
+# Write your code here
+  ascending = 1
+  longest = 0
+  for i in range(1, len(number_array)):
+    if number_array[i] > number_array[i-1]:
+      ascending = ascending + 1
+    if ascending > longest:
+      longest = ascending
+    elif not number_array[i] > number_array[i-1]:
+      ascending = 1
+  return longest
