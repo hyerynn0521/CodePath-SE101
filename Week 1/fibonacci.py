6b664916fac4265863bbcd58c82eccde112c0ce6
hyerynn0521 <52299631+hyerynn0521@users.noreply.github.com>
2021-06-10 22:41:25 -0700

    Update fibonacci.py

--- Week 1/fibonacci.py
+++ Week 1/fibonacci.py
@@ -5,6 +5,11 @@
 # and prints out the nth value of the fibonacci
 # sequence
 #
+"""
+Write a program that prints out the nth value of the Fibonacci sequence without using recursion.
+The Fibonacci sequence is as follows: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ........ where each value is
+the result of adding the previous two integers.
+"""
 def fibonacci(n):
 # Write your code here
   fib_array = [0, 1, 1]
