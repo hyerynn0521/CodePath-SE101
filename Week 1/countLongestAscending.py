a1c3eee534a8c7aebb467ee796a1f4d0f8d95831
hyerynn0521 <52299631+hyerynn0521@users.noreply.github.com>
2021-06-10 22:43:40 -0700

    Update countLongestAscending.py

--- Week 1/countLongestAscending.py
+++ Week 1/countLongestAscending.py
@@ -5,7 +5,10 @@
 # The function accepts INTEGER_ARRAY number_array as parameter.
 #
 """
-
+Count the length of the longest ascending sequence in an array.
+For instance, given [-1, 3, 5, 2, 5, 3, 9, 4, 8, 10, 12, 6]
+the longest ascending sequence is of length 4 - [-1, 3, 5, 2, 5, 3, 9, "4, 8, 10, 12", 6] is the longest set of
+numbers that is continuously increasing.
 """
 def countLongestAscending(number_array):
 # Write your code here
